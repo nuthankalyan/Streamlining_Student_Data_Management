b7ba90f0456068290fc67d38de296fc49cbbd573
kalyan <nuthan317@gmail.com>
2024-05-09 18:09:41 +0530

    added azure_connect.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
     global details
-    details = {'Name' : [],'Roll no' : []} 
+    details = {'Name' : [],'Roll no' : [],'year' : []} 
     if request.method == "POST":
         details['Name'].append(request.form.get('name'))
         details['Roll no'].append(request.form.get('roll'))
+        details['year'].append(request.form.get('year'))
         print(details)
         return render_template('index.html')
     return render_template('index.html')
