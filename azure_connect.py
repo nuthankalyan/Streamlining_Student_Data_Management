5e4af2552106cf28aa17189848a2e09dfc2f04e2
kalyan <nuthan317@gmail.com>
2024-05-11 14:06:07 +0530

    modified app.py and azure_connect.py

--- azure_connect.py
+++ azure_connect.py
@@ -2,6 +2,3 @@ import pypyodbc as odbc
 connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:nuthanserver.database.windows.net,1433;Database=StudentDetails;Uid=CloudSA9645e9f0;Pwd=Nuthan@8106;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 conn = odbc.connect(connection_string)
 cursor = conn.cursor()
-cursor.execute("insert into dbo.test1 values(3,'swaroop')")
-print(cursor.rowcount)
-conn.commit()
