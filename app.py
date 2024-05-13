b4447090e6076e3189f2bec9856a3181d5d375f6
kalyan <nuthan317@gmail.com>
2024-05-13 10:55:50 +0530

    added first.html

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ app = Flask(__name__)
 @app.route('/',methods = ['GET','POST'])
 def home():
     if request.method == 'POST':
-        # from azure_connect import cursor,conn
-        roll = request.form.get('roll')
+        from azure_connect import cursor,conn
+        roll = request.form.get('rollno')
         name = request.form.get('name')
         gender = request.form.get('gender')
         email = request.form.get('email')
         phone = request.form.get('phone')
-        mother = request.form.get('mother')
-        father = request.form.get('father')
+        mother = request.form.get('mothername')
+        father = request.form.get('fathername')
         branch = request.form.get('branch')
         yearofstudy = request.form.get('yearofstudy')
-        yearofgrad = request.form.get('yearofgraduation')
+        yearofgrad = request.form.get('gradyear')
         caste = request.form.get('caste')
-        # cursor.execute(f"insert into dbo.test1 values(?,?)",(roll,name))
-        # conn.commit()
-        return render_template('index.html')
-    return render_template('index.html')
+        cursor.execute(f"insert into dbo.Student values(?,?,?,?,?,?,?,?,?,?,?)",(roll,name,gender,email,phone,mother,father,yearofstudy,yearofgrad,caste,branch))
+        conn.commit()
+        print(roll,name,gender,email,phone,mother,father,branch,yearofstudy,yearofgrad,caste)
+        return render_template('first.html')
+    return render_template('first.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
