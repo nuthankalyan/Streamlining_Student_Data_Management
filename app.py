ea47e0f0cd38f96f64b0d84b3366d372a90d6852
Yara Nuthan kalyan <99541230+nuthankalyan@users.noreply.github.com>
2024-05-13 18:36:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 @app.route('/',methods = ['GET','POST'])
 def home():
     if request.method == 'POST':
-        import pyodbc as odbc
+        import pypyodbc as odbc
         connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:nuthanserver.database.windows.net,1433;Database=StudentDetails;Uid=CloudSA9645e9f0;Pwd=Nuthan@8106;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
         conn = odbc.connect(connection_string)
         cursor = conn.cursor()
