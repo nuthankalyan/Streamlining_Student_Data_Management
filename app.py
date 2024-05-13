87b3f3b0cb96472149ea9c6a9c77bf543785e7c3
kalyan <nuthan317@gmail.com>
2024-05-13 19:21:51 +0530

    modified

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         import pymssql
-        conn = pymssql.connect(server='nuthanserver.database.windows.net', user='CloudSA9645e9f0', password='Nuthan@8106', database='StudentDetails')
+        connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:nuthanserver.database.windows.net,1433;Database=StudentDetails;Uid=CloudSA9645e9f0;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+        conn = pymssql.connect(connection_string)
         cursor = conn.cursor()
         roll = request.form.get('rollno')
         name = request.form.get('name')
